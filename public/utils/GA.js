d4fffea862c597f17892a3a776b7b0b8c3b206bf
CHING-YI HSU <king.swak@gmail.com>
2023-07-13 00:20:32 +0800

    change GA to GA4

--- public/utils/GA.js
+++ public/utils/GA.js
@@ -1,10 +1,9 @@
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga4'
 
 export const initGA = () => {
     ReactGA.initialize('G-3SF2GKZV3L')
 }
 
 export const logPageView = () => {
-    ReactGA.set({ page: window.location.pathname })
-    ReactGA.pageview(window.location.pathname)
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname })
 }
